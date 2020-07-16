eaf04688e5f746a77064580d61fbb60b9b36872b
George Welch <georgewelch@me.com>
2020-07-16 20:07:04 +0100

    "added reverse.js"

--- Recursion/reverse.js
+++ Recursion/reverse.js
@@ -1,1 +1,8 @@
 //given a string, returns the reverse of that string.
+function reverse(string) {
+    if (string.length === 0) return "";
+    let output = string[string.length - 1];
+    return output.concat(reverse(string.slice(0,string.length - 1)));
+}
+
+console.log(reverse("hello"));

97cfca3eae3c966fd3b098806f70d00c520050d0
George Welch <georgewelch@me.com>
2020-07-18 09:58:50 +0100

    added collectStrings

---
+++ Recursion/collectStrings.js
@@ -0,0 +1,27 @@
+//given an object, returns an array of all strings in the object.
+function collectStrings(obj) {
+    let output = [];
+    for (key in obj) {
+        if (typeof obj[key] === "object") output = output.concat(collectStrings(obj[key]));
+        if (typeof obj[key] === "string") output.push(obj[key]);
+    }
+    return output;
+}
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+console.log(collectStrings(obj)); //["foo", "bar", "baz"]

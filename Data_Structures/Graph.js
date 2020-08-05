f61355c44c26dc760e62c8931d12e714d2fec497
George Welch <georgewelch@me.com>
2020-08-05 12:37:13 +0100

    added Traversal methods

--- Data_Structures/Graph.js
+++ Data_Structures/Graph.js
@@ -27,6 +27,61 @@ class Graph {
             delete this.adjacencyList[vertex];
         }
     }
+    depthFirstSearch(start) {
+        let results = [];
+        let visited = {};
+        const recursiveSearch = ((vertex) => {
+            if (!vertex) return null; //base case.
+            results.push(vertex);
+            visited[vertex] = true;
+            for (let neighbor of this.adjacencyList[vertex]) {
+                if (!visited[neighbor]) {
+                    recursiveSearch(neighbor);
+                }
+            }
+        });
+        recursiveSearch(start);
+        return results;
+    }
+    iterativeDFS(start) {
+        let stack = [start];
+        let visited = {};
+        let results = [];
+        let vertex;
+
+        visited[start] = true;
+        while (stack.length) {
+            vertex = stack.pop();
+            results.push(vertex);
+            let adjacencyList = this.adjacencyList[vertex];
+            
+            adjacencyList.forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+             }); 
+            
+        }
+        return results;
+    }
+    breadthFirstSearch(start) {
+        let queue = [start];
+        let results = [];
+        let visited = {};
+        visited[start] = true;
+        while (queue.length) {
+            let dequeued = queue.shift();
+            results.push(dequeued);
+            this.adjacencyList[dequeued].forEach((neighbor) => {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return results;
+    }
 }
 
 let graph = new Graph();
@@ -38,8 +93,14 @@ graph.addEdge("London", "Tokyo");
 graph.addEdge("London", "Paris");
 graph.addEdge("London", "New York");
 graph.addEdge("New York", "Tokyo");
+console.log(graph.depthFirstSearch("London"));
+console.log(graph.iterativeDFS("London"));
+console.log(graph.breadthFirstSearch("London"));
 console.log(graph);
 graph.removeEdge("London", "Tokyo");
 console.log(graph);
 graph.removeVertex("Paris");
 console.log(graph);
+console.log(graph.depthFirstSearch("London"));
+console.log(graph.iterativeDFS("London"));
+console.log(graph.breadthFirstSearch("London"));
